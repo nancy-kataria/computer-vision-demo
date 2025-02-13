edc014bd16afef1e4f00d8e25d4f465f3f66744f
nancy-kataria <katarianancy8@gmail.com>
2025-02-12 23:39:50 -0800

    opening the file

--- vision.py
+++ vision.py
@@ -3,3 +3,12 @@ import io
 
 client = vision.ImageAnnotatorClient.from_service_account_file('/Users/nancy/Downloads/computer-vision-450304-1614d2eda8c3.json')
 
+image_path = "sailboat.jpg"
+
+with io.open(image_path, 'rb') as image_file:
+    content = image_file.read()
+
+image = vision.Image(content=content)
+
+response =client.label_detection(image=image)
+
